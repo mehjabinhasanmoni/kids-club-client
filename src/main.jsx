ce45153d278e2725feccdac64fea2b434f2a51d0
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-06-09 06:59:24 +0600

    Context added

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/routes';
+import AuthProvider from './Providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <div className="max-w-screen-2xl mx-auto">
-        <RouterProvider router={router} />
-     </div>
+      <AuthProvider>
+          <div className="max-w-screen-2xl mx-auto">
+            <RouterProvider router={router} />
+          </div>
+      </AuthProvider>
   </React.StrictMode>,
 )
 
