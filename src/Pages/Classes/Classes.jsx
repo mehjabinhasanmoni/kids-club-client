c9aeb1f2309423df5abff471b82f9ea2b6d4487f
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-06-12 21:33:45 +0600

    Select class by user

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -1,13 +1,75 @@
+import Swal from "sweetalert2";
 import useApprovedClasses from "../../hooks/useApprovedClasses";
+import useAuth from "../../hooks/useAuth";
+import useStudent from "../../hooks/useStudent";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Classes = () => {
   const [approvedclasses] = useApprovedClasses();
   console.log("approved Classes", approvedclasses);
+  //   const [isStudent] = useStudent();
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleAddClass = (aClass) => {
+    console.log("approved Classes in handle", aClass);
+    if (user) {
+      const selectClass = {
+        selectedClassId: aClass._id,
+        classname: aClass.classname,
+        photo: aClass.photo,
+        insname: aClass.insname,
+        price: aClass.price,
+        availseats: aClass.availseats,
+        enrollment: parseFloat(0),
+        email: user.email,
+      };
+      fetch(
+        "https://kids-club-server-production.up.railway.app/selectedclases",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(selectClass),
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.insertedId) {
+            Swal.fire({
+              title: "Class Added Successfull",
+              showClass: {
+                popup: "animate__animated animate__fadeInDown",
+              },
+              hideClass: {
+                popup: "animate__animated animate__fadeOutUp",
+              },
+            });
+          }
+        });
+    } else {
+      Swal.fire({
+        title: "Please login to order this Class",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Login Now",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login", { state: { from: location } });
+        }
+      });
+    }
+  };
+
   return (
-    <div className=" grid grid-cols-4 gap-10 mt-10">
-    {
-        approvedclasses.map((aClass, index) => {
-            return <div key={index} className="card w-96 bg-sky-100 shadow-xl">
+    <div className=" grid grid-cols-4 gap-10 my-10">
+      {approvedclasses.map((aClass, index) => {
+        return (
+          <div key={index} className="card w-96 bg-sky-200 shadow-xl">
             <figure className="px-10 pt-10">
               <img
                 src={aClass.photo}
@@ -22,13 +84,17 @@ const Classes = () => {
               <p>Price : {aClass.price}</p>
 
               <div className="card-actions">
-                <button className="btn btn-secondary">Select Class</button>
+                <button
+                  onClick={() => handleAddClass(aClass)}
+                  className="btn btn-secondary"
+                >
+                  Select Class
+                </button>
               </div>
             </div>
           </div>
-        } )
-    }
-      
+        );
+      })}
     </div>
   );
 };
