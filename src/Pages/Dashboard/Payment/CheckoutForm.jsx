1027f669a4c6c8f1f13a7c3f39dcdbcce1b1fac5
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-06-14 23:06:35 +0600

    available class & enrollment students count

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -4,140 +4,154 @@ import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useAuth from "../../../hooks/useAuth";
 import Swal from "sweetalert2";
 
+const CheckoutForm = ({ classInfo, price }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const { user } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+  const [cardError, setCardError] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const [clientSecret, setClientSecret] = useState("");
+  const [transactionId, setTransactionId] = useState("");
 
-const CheckoutForm = ({ classInfo,price }) => {
-    const stripe = useStripe();
-    const elements = useElements();
-    const { user } = useAuth();
-    const [axiosSecure] = useAxiosSecure();
-    const [cardError, setCardError] = useState('');
-    const [processing, setProcessing] = useState(false);
-    const [clientSecret, setClientSecret] = useState('');
-    const [transactionId, setTransactionId] = useState('');
+  useEffect(() => {
+    if (price > 0) {
+      axiosSecure.post("/create-payment-intent", { price }).then((res) => {
+        console.log(res.data.clientSecret);
+        setClientSecret(res.data.clientSecret);
+      });
+    }
+  }, [price, axiosSecure]);
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    useEffect(() => {
-        if (price > 0) {
-            axiosSecure.post('/create-payment-intent', { price })
-                .then(res => {
-                    console.log(res.data.clientSecret)
-                    setClientSecret(res.data.clientSecret);
-                })
-        }
-    }, [price, axiosSecure])
-
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        if (!stripe || !elements) {
-            return
-        }
-
-        const card = elements.getElement(CardElement);
-
-        if (card === null) {
-            return
-        }
-
-        const { error } = await stripe.createPaymentMethod({
-            type: 'card',
-            card
-        })
-
-        if (error) {
-            console.log('error', error)
-            setCardError(error.message);
-        }
-        else {
-            setCardError('');
-            
-        }
-
-        setProcessing(true)
-
-        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
-            clientSecret,
-            {
-                payment_method: {
-                    card: card,
-                    billing_details: {
-                        email: user?.email || 'unknown',
-                        name: user?.displayName || 'anonymous'
-                    },
-                },
-            },
-        );
-
-        if (confirmError) {
-            console.log(confirmError);
-        }
-        console.log('payment intent', paymentIntent)
-        setProcessing(false)
-
-        if (paymentIntent.status === 'succeeded') {
-            setTransactionId(paymentIntent.id);
-            // save payment information to the server
-            const payment = {
-                email: user?.email,
-                transactionId: paymentIntent.id,
-                price,
-                date: new Date(),
-                classname: classInfo.classname,
-                selectedClassesId: classInfo._id,
-                paymentSelectClassId:classInfo.selectedClassId,
-                status:'enrollment'
-
-            }
-            axiosSecure.post('/payments', payment)
-            .then(res => {
-                console.log(res.data);
-                if (res.data.result.insertedId) {
-                    Swal.fire({
-                        position: 'top-end',
-                        icon: 'success',
-                        title: 'Payment successfully Done',
-                        showConfirmButton: false,
-                        timer: 1500
-                      })
-                }
-            })
-    
-        }
-
-
-
+    if (!stripe || !elements) {
+      return;
     }
 
+    const card = elements.getElement(CardElement);
 
-    return (
-        <>
-        <form className="w-2/3 m-8" onSubmit={handleSubmit}>
-                <CardElement
-                    options={{
-                        style: {
-                            base: {
-                                fontSize: '16px',
-                                color: '#424770',
-                                '::placeholder': {
-                                    color: '#aab7c4',
-                                },
-                            },
-                            invalid: {
-                                color: '#9e2146',
-                            },
-                        },
-                    }}
-                />
-                <button className="btn btn-primary btn-sm mt-4" type="submit" disabled={!stripe|| !clientSecret || processing}>
-                    Pay
-                </button>
-            </form>
-            {cardError && <p className="text-red-600 ml-8">{cardError}</p>}
-            {transactionId && <p className="text-green-500">Transaction complete with transactionId: {transactionId}</p>}
-            
-        </>
-    );
+    if (card === null) {
+      return;
+    }
+
+    const { error } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("error", error);
+      setCardError(error.message);
+    } else {
+      setCardError("");
+    }
+
+    setProcessing(true);
+
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "unknown",
+            name: user?.displayName || "anonymous",
+          },
+        },
+      });
+
+    if (confirmError) {
+      console.log(confirmError);
+    }
+    console.log("payment intent", paymentIntent);
+    setProcessing(false);
+
+    if (paymentIntent.status === "succeeded") {
+      setTransactionId(paymentIntent.id);
+      var availseats = 0;
+      var totalenrolledstudents = 0;
+      //   available seat update
+      fetch(
+        `https://kids-club-server-production.up.railway.app/singleClass/${classInfo.selectedClassId}`,
+        {
+          method: "GET",
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("class data", data?.availseats);
+          availseats = parseInt(data?.availseats) - 1;
+          totalenrolledstudents = parseInt(data?.totalenrolledstudents) + 1;
+          // save payment information to the server
+          const payment = {
+            paymentInfo: {
+              email: user?.email,
+              transactionId: paymentIntent.id,
+              price,
+              date: new Date(),
+              classname: classInfo.classname,
+              selectedClassesId: classInfo._id,
+              paymentSelectClassId: classInfo.selectedClassId,
+            },
+            classInfo: { ...classInfo },
+            classes: {
+              availseats: availseats,
+              totalenrolledstudents: totalenrolledstudents,
+              selectedClassId: classInfo.selectedClassId,
+            },
+          };
+          axiosSecure.post("/payments", payment).then((res) => {
+            console.log(res.data);
+            if (res.data.result.insertedId) {
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Payment successfully Done",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }
+          });
+        });
+    }
+  };
+
+  return (
+    <>
+      <form className="w-2/3 m-8" onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-primary btn-sm mt-4"
+          type="submit"
+          disabled={!stripe || !clientSecret || processing}
+        >
+          Pay
+        </button>
+      </form>
+      {cardError && <p className="text-red-600 ml-8">{cardError}</p>}
+      {transactionId && (
+        <p className="text-green-500">
+          Transaction complete with transactionId: {transactionId}
+        </p>
+      )}
+    </>
+  );
 };
 
 export default CheckoutForm;
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CheckoutForm from "./CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
@@ -11,32 +11,35 @@ const Payment = () => {
   const [classInfo, setClassInfo] = useState({});
   // const price = classInfo?.price ? parseFloat(classInfo?.price) : 0;
   const price = classInfo?.price;
-  fetch(
-    `https://kids-club-server-production.up.railway.app/selectedclass/${id}`,
-    {
-        method : 'GET'
-    })
-    .then((res) => res.json())
-    .then((data) => {
+  useEffect(() => {
+    fetch(
+      `https://kids-club-server-production.up.railway.app/selectedclass/${id}`,
+      {
+        method: "GET",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
         setClassInfo(data);
-    })
-    .catch(error =>{
-      console.log(error);
-    })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
-      <h2 className="text-center text-3xl tracking-wider my-10 ">Payment for</h2>
+      <h2 className="text-center text-3xl tracking-wider my-10 ">
+        Payment for
+      </h2>
       <div className="w-full p-5  border-2 border-black bg-blue-500 flex justify-around items-center">
         <p>Class Name : {classInfo.classname}</p>
         <p>Amount : {classInfo.price}</p>
       </div>
 
-      <Elements stripe = {stripePromise}>
-            <CheckoutForm classInfo={classInfo} price={price}></CheckoutForm>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm classInfo={classInfo} price={price}></CheckoutForm>
       </Elements>
-      
-
     </>
   );
 };
